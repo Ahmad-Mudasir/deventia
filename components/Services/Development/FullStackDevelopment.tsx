cfac10839bbb3dd305623c0579eb050ae259ed8a
amirsuliman01 <amirsuliman01@gmail.com>
2024-07-23 22:34:15 +0500

    new changes

--- components/Services/Development/FullStackDevelopment.tsx
+++ components/Services/Development/FullStackDevelopment.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import fullStackimg from "../../../assets/images/FullStackDevelopment.jpg";
-import Image from "next/image";
+import React from 'react';
+import fullStackimg from '../../../assets/images/FullStackDevelopment.jpg';
+import Image from 'next/image';
 
 function FullStackDevelopment() {
   return (
     <div className="grid grid-cols-12 h-[480px] px-20 py-4">
       <div className="left col-span-7 h-full pr-24 flex flex-col gap-6">
         <div className="heading text-[40px] font-bold">
-          <h1>Welcome to Full </h1>{" "}
-          <h1>Stack Development</h1>
+          <h1>Welcome to Full </h1> <h1>Stack Development</h1>
         </div>
         <div className="description flex flex-col justify-center items-center gap-5">
           <div className="container1 text-md">
@@ -16,14 +15,14 @@ function FullStackDevelopment() {
             tailored services designed to meet your unique business needs. From
             backend and frontend development to database management and API
             integration, we offer a comprehensive suite of solutions to help you
-            thrive in today's competitive landscape.
+            thrive in today&aposs competitive landscape.
           </div>
           <div className="container2 text-md">
             MERN stack development refers to using four key technologies for
             building web applications: MongoDB (a NoSQL database), Express.js (a
             backend web framework), React (a frontend library for building user
             interfaces), and Node.js (a runtime environment for executing
-            JavaScript code outside the browser). It's quite popular for its
+            JavaScript code outside the browser). It&aposs quite popular for its
             flexibility and efficiency in developing full-stack applications
           </div>
         </div>
@@ -33,9 +32,8 @@ function FullStackDevelopment() {
           src={fullStackimg}
           alt="Landscape picture"
           className="object-cover h-full w-full"
-        //   width={580}
-        //   height={480}
-
+          //   width={580}
+          //   height={480}
         />
       </div>
     </div>
