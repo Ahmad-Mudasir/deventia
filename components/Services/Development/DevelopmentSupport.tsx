8082160c27812041c3cdc8be671801c63dfe3160
unknown <Farman-Ali-1>
2024-07-25 00:16:01 -0700

    development page is responsive

--- components/Services/Development/DevelopmentSupport.tsx
+++ components/Services/Development/DevelopmentSupport.tsx
@@ -1,47 +1,39 @@
 import React from "react";
 import developmentSupportimg from "../../../assets/images/DevelopmentSupport.jpg";
 import Image from "next/image";
-import { IoIosCheckboxOutline } from "react-icons/io";
+import { FaRegSquareCheck } from "react-icons/fa6";
 function DevelopmentSupport() {
   return (
-    <div className="grid grid-cols-12 h-[480px] w-auto px-16 py-4">
-      <div className="p-5 col-span-6">
-        <Image
-          src={developmentSupportimg}
-          alt="banner"
-          className="object-cover h-[400px] w-full"
-        />
+    <section className="flex gap-8 flex-col md:flex-row p-[5%]">
+      <Image
+        src={developmentSupportimg}
+        alt="design service"
+        className="w-full md:w-[48%]"
+      />
+      <div>
+        <h1 className="text-2xl lg:text-4xl font-bold w-full mb-6">
+          Services By Our Experts
+        </h1>
+        <h2 className="text-xl lg:text-2xl flex gap-2 items-center font-semibold mt-8">
+          <FaRegSquareCheck className="text-[#8A88EC]" size={22} />
+          24/7 Assistance
+        </h2>
+        <p className="ml-[30px] text-[#E0E5F3] text-base mt-2 leading-8">
+          Round-the-clock support ensures that assistance is available 24/7 to
+          address any development issues immediately, minimizing downtime and
+          enhancing productivity.
+        </p>
+        <h2 className="text-xl lg:text-2xl flex gap-2 items-center font-semibold mt-4">
+          <FaRegSquareCheck className="text-[#8A88EC]" size={22} />
+          Code Reviews
+        </h2>
+        <p className="ml-[30px] text-[#E0E5F3] text-base mt-2 leading-8">
+          Detailed code reviews ensure that code is thoroughly examined for
+          performance optimizations, best practices, and potential issues before
+          deployment.
+        </p>
       </div>
-      <div className="col-span-6 h-full pl-16 flex flex-col gap-6">
-        <div className="heading text-4xl my-3 font-bold">
-          <h1>Development Support</h1>
-        </div>
-        <div className="description flex flex-col  justify-center items-center gap-5">
-          <div className="grid grid-cols-6">
-            <IoIosCheckboxOutline className="h-10 w-10 text-[#8A88EC] col-span-1" />
-            <div className="1st col-span-5 flex flex-col gap-4">
-              <h1 className="text-xl font-bold">24/7 Assistance</h1>
-              <p className="text-sm leading-8">
-                Round-the-clock support ensures that assistance is available
-                24/7 to address any development issues immediately, minimizing
-                downtime and enhancing productivity.
-              </p>
-            </div>
-          </div>
-          <div className="grid grid-cols-6">
-            <IoIosCheckboxOutline className="h-10 w-10 text-[#8A88EC] col-span-1" />
-            <div className="1st col-span-5 flex flex-col gap-4">
-              <h1 className="text-xl font-bold">Code Reviews</h1>
-              <p className="text-sm leading-8">
-                Detailed code reviews ensure that code is thoroughly examined
-                for performance optimizations, best practices, and potential
-                issues before deployment.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 }
 
