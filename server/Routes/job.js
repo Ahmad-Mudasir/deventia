2b84d7cbed876405322c2588e7a6b3fe035ce370
imrankhan992 <imrankohistani65@gmail.com>
2024-07-26 17:17:12 +0500

    contact us api complete

--- server/Routes/job.js
+++ server/Routes/job.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { postJob } = require("../controller/Jobs/post.controller");
-// import {postJob} = require("./server/Jobs/post.controller.js")
+const { postJob, getJobs, updateJob, deleteJob } = require("../controller/Jobs/post.controller");
+
 const router = express.Router();
 router.post("/job/post", postJob)
+router.get("/job/get", getJobs)
+router.put("/job/update/:id", updateJob)
+router.delete("/job/delete/:id", deleteJob)
 
 
 module.exports = router
