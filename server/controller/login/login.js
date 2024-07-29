618a25715b625aca901181fbf5c76ccf8f94bd68
unknown <Farman-Ali-1>
2024-07-28 23:31:30 -0700

    login api is created

--- server/controller/login/login.js
+++ server/controller/login/login.js
@@ -1,21 +1,29 @@
-const User = require('../../Model/userModel')
-const login = async(req,res)=>{
+const User = require('../../Model/userModel');
+
+const login = async (req, res) => {
     try {
-        const {email,password}= req.body;
+        const { email, password } = req.body;
+
+        // Check if the credentials match the admin credentials
+        if (email === 'Admin@gmail.com' && password === 'adminabc') {
+            return res.send('Admin Login Successful');
+        }
+
+        // If not admin, proceed with user login
         const user = await User.findOne({ email: email });
-        if(user){
+        if (user) {
             if (user.password === password) {
-                res.send('Login Successful');
+                res.send('User Login Successful');
             } else {
                 res.send('Invalid email or password');
             }
-        }else{
-            res.send('user not found ');
+        } else {
+            res.send('User not found');
         }
     } catch (error) {
         console.log(error);
         res.status(500).send('Server error');
     }
-}
+};
 
 module.exports = login;
