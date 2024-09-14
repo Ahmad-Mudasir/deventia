e4eb3a11249cb2d6a851a8f16f03ab45c45c001b
amirsuliman01 <amirsuliman01@gmail.com>
2024-09-14 11:28:20 +0500

    error removed

--- Frontend/components/home/AnimatedText.tsx
+++ Frontend/components/home/AnimatedText.tsx
@@ -41,41 +41,95 @@ import Image from 'next/image';
 
 const AnimatedText = () => {
   const icons = [
-    <FaLaptop className="text-[--home-hero-animation-text]" />,
-    <Image src={figma} alt="figma" />,
-    <FaAmericanSignLanguageInterpreting className="text-[--home-hero-animation-text]" />,
-    <Image src={reactjs} alt="reactjs" />,
-    <FaPeopleRoof className="text-[--home-hero-animation-text]" />,
-    <FaMagnifyingGlassChart className="text-[--home-hero-animation-text]" />,
-    <Image src={bootstrap} alt="bootstrap" />,
-    <FaHospitalUser className="text-[--home-hero-animation-text]" />,
-    <Image src={material} alt="material" />,
-    <FaAddressCard className="text-[--home-hero-animation-text]" />,
-    <FaCircleDollarToSlot className="text-[--home-hero-animation-text]" />,
-    <Image src={nextjs} alt="next.js" />,
-    <FaMicrophone className="text-[--home-hero-animation-text]" />,
-    <Image src={redux} alt="redux" />,
-    <GrCloudSoftware className="text-[--home-hero-animation-text]" />,
-    <GrCompliance className="text-[--home-hero-animation-text]" />,
-    <GrTechnology className="text-[--home-hero-animation-text]" />,
-    <MdAccountBalance className="text-[--home-hero-animation-text]" />,
-    <HiSpeakerphone className="text-[--home-hero-animation-text]" />,
-    <Image src={tialwind} alt="tialwind" />,
-    <FaFirstAid className="text-[--home-hero-animation-text]" />,
-    <LiaCommentDollarSolid className="text-[--home-hero-animation-text]" />,
-    <GiSatelliteCommunication className="text-[--home-hero-animation-text]" />,
-    <FaSackDollar className="text-[--home-hero-animation-text]" />,
-    <Image src={github} alt="github" />,
-    <FaTablet className="text-[--home-hero-animation-text]" />,
-    <FaGamepad className="text-[--home-hero-animation-text]" />,
-    <Image src={icon} alt="icon" />,
-    <FaMobile className="text-[--home-hero-animation-text]" />,
-    <FaTripadvisor className="text-[--home-hero-animation-text]" />,
-    <FaMousePointer className="text-[--home-hero-animation-text]" />,
-    <FaMouse className="text-[--home-hero-animation-text]" />,
-    <Image src={graphql} alt="graphql" />,
-    <FaSafari className="text-[--home-hero-animation-text]" />,
-    <FaChrome className="text-[--home-hero-animation-text]" />,
+    <FaLaptop key="laptop" className="text-[--home-hero-animation-text]" />,
+    <Image key="figma" src={figma} alt="figma" />,
+    <FaAmericanSignLanguageInterpreting
+      key="asl"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="reactjs" src={reactjs} alt="reactjs" />,
+    <FaPeopleRoof
+      key="people-roof"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <FaMagnifyingGlassChart
+      key="magnifying-glass"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="bootstrap" src={bootstrap} alt="bootstrap" />,
+    <FaHospitalUser
+      key="hospital-user"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="material" src={material} alt="material" />,
+    <FaAddressCard
+      key="address-card"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <FaCircleDollarToSlot
+      key="circle-dollar"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="nextjs" src={nextjs} alt="next.js" />,
+    <FaMicrophone
+      key="microphone"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="redux" src={redux} alt="redux" />,
+    <GrCloudSoftware
+      key="cloud-software"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <GrCompliance
+      key="compliance"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <GrTechnology
+      key="technology"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <MdAccountBalance
+      key="account-balance"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <HiSpeakerphone
+      key="speakerphone"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="tailwind" src={tialwind} alt="tailwind" />,
+    <FaFirstAid
+      key="first-aid"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <LiaCommentDollarSolid
+      key="comment-dollar"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <GiSatelliteCommunication
+      key="satellite"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <FaSackDollar
+      key="sack-dollar"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <Image key="github" src={github} alt="github" />,
+    <FaTablet key="tablet" className="text-[--home-hero-animation-text]" />,
+    <FaGamepad key="gamepad" className="text-[--home-hero-animation-text]" />,
+    <Image key="scss" src={icon} alt="scss" />,
+    <FaMobile key="mobile" className="text-[--home-hero-animation-text]" />,
+    <FaTripadvisor
+      key="tripadvisor"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <FaMousePointer
+      key="mouse-pointer"
+      className="text-[--home-hero-animation-text]"
+    />,
+    <FaMouse key="mouse" className="text-[--home-hero-animation-text]" />,
+    <Image key="graphql" src={graphql} alt="graphql" />,
+    <FaSafari key="safari" className="text-[--home-hero-animation-text]" />,
+    <FaChrome key="chrome" className="text-[--home-hero-animation-text]" />,
   ];
 
   useEffect(() => {
@@ -105,8 +159,8 @@ const AnimatedText = () => {
       {['Deventia', 'Scripting', 'Future', 'Technologies'].map(
         (word, wordIndex) => (
           <div
-            className={` ${styles.word} gap-0.5 space-y-0.5`}
-            key={wordIndex}
+            className={`${styles.word} gap-0.5 space-y-0.5`}
+            key={`word-${wordIndex}`}
           >
             {word.split('').map((letter, index) => {
               const iconElement = icons[globalIconIndex % icons.length];
@@ -114,7 +168,7 @@ const AnimatedText = () => {
 
               return (
                 <div
-                  key={index}
+                  key={`${word}-${index}`}
                   className={`${styles.box} size-[30px] lg:size-[50px]`}
                   data-letter={letter}
                 >
